0423da8ee0231bdfc2d1d7fc30e0b146f7a279ec
litovcenkoartem363-sudo <litovcenkoartem363@gmail.com>
2026-06-02 14:10:55 +0300

    Update index.tsx

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/custom-setup');
+    router.replace('/simple');
   }, [router]);
 
   return (
